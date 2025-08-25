16af851493dcfe82a5f6fe7fbec7862243df4101
Farhang Bakhshi <farhang.bakhshi@gmail.com>
2025-08-25 14:49:27 +0330

    Refactor webhook handling to use logging and subprocess for deployment

--- app/routes.py
+++ app/routes.py
@@ -1,19 +1,34 @@
+import subprocess
 from flask import Blueprint, request, jsonify
+from services import Services as service
+import logging
 
-bp = Blueprint('main', __name__)
+bp = Blueprint("main", __name__)
+
 
 @bp.route("/webhook", methods=["POST"])
 def webhook():
     if request.is_json:
         payload = request.get_json()
 
-        print("✅ Webhook Received!")
-        repo_name = payload.get('repository', {}).get('full_name', 'N/A')
-        print(f"Repository: {repo_name}")
+        processed_data = service.process_webhook(payload)
+        subprocess.run(
+            ["/home/farhang/source_codes/garcon/deploy.sh", processed_data["repo_url"]],
+            check=True,
+        )
+
+        # Configure logging to write to ../logs/app.log
+        logging.basicConfig(
+            filename="../logs/app.log",
+            level=logging.INFO,
+            format="%(asctime)s - %(levelname)s - %(message)s",
+        )
+        logging.info(f"Webhook Received! Repository: {processed_data['repo_name']}")
 
         return jsonify(message="Webhook received successfully"), 200
     else:
         return jsonify(error="Request was not JSON"), 400
+
 
 @bp.route("/")
 def index():
