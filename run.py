2d44598f20fcf38668d4e2c5ee63deb743d5cc0e
Farhang Bakhshi <farhang.bakhshi@gmail.com>
2025-08-25 15:44:39 +0330

    Configure app to run on specified host and port from configuration

--- run.py
+++ run.py
@@ -3,4 +3,6 @@ from app import create_app
 app = create_app()
 
 if __name__ == "__main__":
-    app.run()
+    host = app.config.get("SERVICE_HOST", "127.0.0.1")
+    port = app.config.get("SERVICE_PORT", 5000)
+    app.run(host=host, port=port)
